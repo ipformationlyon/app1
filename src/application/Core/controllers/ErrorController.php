952310af0b5c480e08c9372d1bd3f1e5d88d99de
jb <jb@ipformation.local>
2014-01-08 10:33:19 +0100

    Ajout de HTML5Boilerplate

--- src/application/Core/controllers/ErrorController.php
+++ src/application/Core/controllers/ErrorController.php
@@ -13,6 +13,7 @@ class ErrorController extends Zend_Controller_Action
     
     public function errorAction()
     {
-        
+        $errorHandler = $this->_getParam('error_handler');
+        $this->view->exception = $errorHandler->exception;
     }
 }
