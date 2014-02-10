981da00973fa5cff7c53816afb6dddc339c4ed52
jb <jb@ipformation.local>
2014-02-10 15:09:02 +0100

    Ajout des Models Article Author Content User Role

---
+++ src/application/Core/models/Author.php
@@ -0,0 +1,49 @@
+<?php 
+
+class Core_Model_Author implements Zend_Acl_Role_Interface, Zend_Acl_Resource_Interface
+{
+    private $authorId;
+    private $authorName;
+	/**
+     * @return the $authorId
+     */
+    public function getAuthorId()
+    {
+        return $this->authorId;
+    }
+
+    public function getRoleId(){
+        return 'author';
+    }
+    
+    public function getResourceId(){
+        return 'author';
+    }
+    
+	/**
+     * @param field_type $authorId
+     */
+    public function setAuthorId($authorId)
+    {
+        $this->authorId = $authorId;
+        return $this;
+    }
+
+	/**
+     * @return the $authorName
+     */
+    public function getAuthorName()
+    {
+        return $this->authorName;
+    }
+
+	/**
+     * @param field_type $authorName
+     */
+    public function setAuthorName($authorName)
+    {
+        $this->authorName = $authorName;
+        return $this;
+    }
+
+}
---
+++ src/application/Core/models/User.php
@@ -0,0 +1,59 @@
+<?php 
+
+class Core_Model_User implements Zend_Acl_Role_Interface{
+    
+    private $roleId;
+    private $userId;
+    private $userLogin;
+    
+	/**
+     * @return the $userId
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @see Zend_Acl_Role_Interface::getRoleId()
+     */
+    public function getRoleId(){
+        return $this->roleId;
+    }
+
+	/**
+     * @param field_type $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+
+	/**
+     * @return the $userLogin
+     */
+    public function getUserLogin()
+    {
+        return $this->userLogin;
+    }
+
+	/**
+     * @param field_type $userLogin
+     */
+    public function setUserLogin($userLogin)
+    {
+        $this->userLogin = $userLogin;
+        return $this;
+    }
+
+    /**
+     * @param unknown $roleId
+     * @return Core_Model_User
+     */
+    public function setRoleId($roleId)
+    {
+        $this->roleId = $roleId;
+        return $this;
+    }
+}
